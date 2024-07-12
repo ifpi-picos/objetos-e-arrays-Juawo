835b420d5ee5533cbff181e853d87b7f784df72a
João Pedro R. Medeiros de Amorim <126190351+Juawo@users.noreply.github.com>
2024-07-12 15:29:42 -0300

    Primeiro envio das Atividades sobre Objetos

---
+++ atividade1.js
@@ -0,0 +1,10 @@
+const aluno = {
+  nome : 'Artur Martins',
+  idade : '18' ,
+  curso : 'Análise e Desenvolvimento de Sistemas',
+  notas : [10.0, 4.0, 3.5],
+}
+
+for(let propiedade in aluno){
+  console.log(`${propiedade}: ${aluno[propiedade]}`)
+}

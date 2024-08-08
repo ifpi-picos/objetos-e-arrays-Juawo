5075ee8a96ecdea530b2bf7d9551b0c7f7614fb0
Joao Pedro <joaopedrojp2703@gmail.com>
2024-08-07 23:20:02 -0300

    teste

--- modularizacao/exercicio7/index.js
+++ modularizacao/exercicio7/index.js
@@ -3,7 +3,7 @@ import convert from "convert";
 const dias = parseInt(prompt('Digite uma quantidade de dias para ser convertido em minutos : '))
 const minutos = convert(dias,'days').to('minutes')
 console.log(`${dias} dias em minutos é ${minutos} min`)
-
+console.Console()
 const gigaBytes = parseInt(prompt('Digite uma quantidade de Gigabytes para ser convertido em bytes : '))
 const bytes = convert(gigaBytes,'GiB').to('bytes')
 console.log(`${gigaBytes} GB em bytes é ${bytes} bytes`)
